0ba9e5cec930854ca8c4290ee78ad2b96b4cf9e2
Eric Schles <ericschles@gmail.com>
2017-08-10 19:02:53 -0400

    whatever, giving up on life

--- application_container.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-application_object = Flask(__name__)
-
-@application_object.route("/", methods=["GET","POST"])
-def hello():
-    return "Hello, World!"
-
-application_object.run()
